4e7d1ee87180c5fdb1791cd457488de790b867d4
sik2 <devpang20@gmail.com>
2023-08-07 11:27:01 +0900

    feat: add entity

--- src/main/java/com/cojar/market/member/entity/Member.java
+++ src/main/java/com/cojar/market/member/entity/Member.java
@@ -1,7 +1,28 @@
 package com.cojar.market.member.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
 
 @Entity
 public class Member {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private long id;
+    @Column(unique = true)
+    private String username;
+    private String password;
+    private String nickname;
+    private String email;
+    @CreatedDate
+    private LocalDateTime createDate;
+    @LastModifiedDate
+    private LocalDateTime modifyDate;
 }
