8ace61a6b063bb75c9673ceb8849549a09a87890
sik2 <devpang20@gmail.com>
2023-08-08 11:40:32 +0900

    feat: add password encoder

--- src/main/java/com/cojar/market/member/service/MemberService.java
+++ src/main/java/com/cojar/market/member/service/MemberService.java
@@ -3,17 +3,20 @@ package com.cojar.market.member.service;
 import com.cojar.market.member.entity.Member;
 import com.cojar.market.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class MemberService {
     private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
+
     public Member join(String username, String password, String email, String nickname) {
 
         Member member = new Member();
             member.setUsername(username);
-            member.setPassword(password);
+            member.setPassword(passwordEncoder.encode(password));
             member.setEmail(email);
             member.setNickname(nickname);
         memberRepository.save(member);
