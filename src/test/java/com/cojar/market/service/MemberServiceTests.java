92a9945d918bb9f438dc95ec6d31a8c543df9a11
sik2 <devpang20@gmail.com>
2023-08-08 09:30:59 +0900

    test: member

---
+++ src/test/java/com/cojar/market/service/MemberServiceTests.java
@@ -0,0 +1,27 @@
+package com.cojar.market.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+public class MemberServiceTests {
+
+    @Autowired
+    private MemberService memberService;
+
+    @Test
+    @DisplayName("회원가입")
+    void t1 () {
+        String username = "user1";
+        String password = "1234";
+        String email = "test@test.com";
+        String nickname = "tester";
+
+        memberService.join(username, password, email, nickname);
+
+    }
+}
