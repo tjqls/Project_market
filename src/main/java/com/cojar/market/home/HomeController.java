f435270df15ee8221073b6f5b34efb7b07d328bc
sik2 <devpang20@gmail.com>
2023-08-08 10:47:50 +0900

    feat: setting security

--- src/main/java/com/cojar/market/home/HomeController.java
+++ src/main/java/com/cojar/market/home/HomeController.java
@@ -1,5 +1,6 @@
 package com.cojar.market.home;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
